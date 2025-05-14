d0a7b3e079eb08bd3c82558901c58a7c6640f1fc
Austin Lee <haulrest@gmail.com>
2025-05-15 01:25:32 +0900

    Fix

---
+++ sample-files/1-02/install-microk8s.sh
@@ -0,0 +1,24 @@
+HOSTNAME="ubuntu-temp-vmware"
+
+#!/bin/bash
+
+# Install MicroK8s
+sudo snap install microk8s --classic
+
+# Set user permissions
+sudo usermod -a -G microk8s austin
+sudo chown -R austin ~/.kube
+
+# Set alias
+echo "alias kubectl='microk8s kubectl'" >> ~/.bashrc
+. ~/.bashrc
+
+# Set node label
+microk8s kubectl label node $HOSTNAME env=vmware
+
+# Check node labels and MicroK8s status
+microk8s kubectl get nodes --show-labels
+microk8s status
+
+# Set new group for command line
+newgrp microk8s
