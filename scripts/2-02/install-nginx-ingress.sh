7a4eb11afc817e4228fb6ac485d143a1aefdb7e2
Austin Lee <haulrest@gmail.com>
2025-05-24 22:35:25 +0900

    add

--- scripts/2-02/install-nginx-ingress.sh
+++ scripts/2-02/install-nginx-ingress.sh
@@ -6,6 +6,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --version ${version} \
-    --set controller.service.loadBalancerIP=192.168.10.180 
+    --set controller.service.loadBalancerIP=192.168.10.180 \
+    --create-namespace
 
 echo "Successfully installed ingress-nginx"
