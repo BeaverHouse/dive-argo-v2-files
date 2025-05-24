56c49b610c11d03cc656efc80dd30117e85e5d9f
Austin Lee <haulrest@gmail.com>
2025-05-24 22:34:36 +0900

    add

--- scripts/2-01/install-metallb.sh
+++ scripts/2-01/install-metallb.sh
@@ -6,6 +6,9 @@ microk8s kubectl apply -f charts/oss-metallb/namespace.yaml
 
 microk8s kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v${version}/config/manifests/metallb-native.yaml
 
+# Wait for install
+sleep 30
+
 microk8s kubectl apply -f charts/oss-metallb/ipconfig.yaml
 
 echo "Successfully installed MetalLB"
