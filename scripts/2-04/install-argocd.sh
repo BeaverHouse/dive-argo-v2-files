37190191a5bfbe33acd0717325ede6a341b0093b
Austin Lee <haulrest@gmail.com>
2025-05-25 01:19:21 +0900

    add

--- scripts/2-04/install-argocd.sh
+++ scripts/2-04/install-argocd.sh
@@ -7,7 +7,7 @@ microk8s kubectl apply -f charts/oss-argocd/externalsecret.yaml
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-helm install argocd argo/argo-cd --namespace argo-project --create-namespace \
+helm upgrade --install argocd argo/argo-cd --namespace argo-project --create-namespace \
     --values charts/oss-argocd/values.yaml \
     --create-namespace \
     --version $version
