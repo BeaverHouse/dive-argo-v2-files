34c43647c3f0ae857255fde8df339871cb1ac174
Austin Lee <haulrest@gmail.com>
2025-07-06 00:51:01 +0900

    Add controller kind

--- scripts/2-02/install-nginx-ingress.sh
+++ scripts/2-02/install-nginx-ingress.sh
@@ -6,6 +6,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --version ${version} \
+    --set controller.kind=DaemonSet \
     --set controller.service.loadBalancerIP=192.168.0.180 \
     --set controller.progressDeadlineSeconds=null \
     --create-namespace
